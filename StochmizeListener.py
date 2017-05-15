776714abbaca1d967c3f13d00a24f1e3c5a479e6
oserasoa <oserasoa@unal.edu.co>
2017-05-15 00:06:42 -0500

    Reconocimiento de definicion de variables(vars_def)

--- StochmizeListener.py
+++ StochmizeListener.py
@@ -3,16 +3,9 @@ from antlr4 import *
 
 # This class defines a complete listener for a parse tree produced by StochmizeParser.
 class StochmizeListener(ParseTreeListener):
-
     # Enter a parse tree produced by StochmizeParser#program.
     def enterProgram(self, ctx):
-        #pass
-        print("%s" % ctx.ID(0))
-        """
-        for child in ctx.getChildren():
-            print(child)
-        """
-        #return ctx.model(0)
+        pass
 
     # Exit a parse tree produced by StochmizeParser#program.
     def exitProgram(self, ctx):
@@ -21,9 +14,6 @@ class StochmizeListener(ParseTreeListener):
 
     # Enter a parse tree produced by StochmizeParser#model.
     def enterModel(self, ctx):
-        #return ctx.vars_def()
-        #print("enterModel")
-        #print(ctx.vars_def())
         pass
 
     # Exit a parse tree produced by StochmizeParser#model.
@@ -33,11 +23,7 @@ class StochmizeListener(ParseTreeListener):
 
     # Enter a parse tree produced by StochmizeParser#vars_def.
     def enterVars_def(self, ctx):
-        #return ctx.fixed()
-        print(ctx.ID())
-        #print(ctx.ID(1))
-        #print(ctx.ID(2))
-        
+        pass
 
     # Exit a parse tree produced by StochmizeParser#vars_def.
     def exitVars_def(self, ctx):
@@ -46,19 +32,16 @@ class StochmizeListener(ParseTreeListener):
 
     # Enter a parse tree produced by StochmizeParser#fixed.
     def enterFixed(self, ctx):
-        #return ctx.NUMBER
-        print(ctx.NUMBER())
-        
+        pass
 
     # Exit a parse tree produced by StochmizeParser#fixed.
     def exitFixed(self, ctx):
-        
         pass
 
 
     # Enter a parse tree produced by StochmizeParser#random.
     def enterRandom(self, ctx):
-        print(ctx)
+        pass
 
     # Exit a parse tree produced by StochmizeParser#random.
     def exitRandom(self, ctx):
@@ -67,8 +50,7 @@ class StochmizeListener(ParseTreeListener):
 
     # Enter a parse tree produced by StochmizeParser#normal.
     def enterNormal(self, ctx):
-        print(ctx.NORMAL())
-        print(ctx.normal_params().NUMBER(1))
+        pass
 
     # Exit a parse tree produced by StochmizeParser#normal.
     def exitNormal(self, ctx):
--- test.py
+++ test.py
@@ -2,23 +2,55 @@ from antlr4 import *
 from StochmizeLexer import StochmizeLexer
 from StochmizeListener import StochmizeListener
 from StochmizeParser import StochmizeParser
+import antlr4
 import sys
 
-"""
-class StochmizePrintListener(StochmizeListener):
-    def enterProgram(self, ctx):
-        print("%s" % ctx.ID('T_0'))
+varIds = []
+varNum = []
 
-	def enterModel(self, ctx):
-		print("%s" % ctx.VARS_DEF(0))
-"""
+normal_params_m = []
+normal_params_v = []
+
+unif_params_a = []
+unif_params_b = []
+class StochmizePrintListener(StochmizeListener):
+
+	def enterProgram(self, ctx):
+		print(ctx.ID(0))
+	# Enter a parse tree produced by StochmizeParser#vars_def.
+	def enterVars_def(self, ctx):
+		
+		
+		print("enterVars_def")
+		for child in ctx.getChildren():
+			if isinstance(child , antlr4.tree.Tree.TerminalNodeImpl):
+				if child.getText() != ';':
+					varIds.append(child.getText())
+
+			if isinstance(child , StochmizeParser.FixedContext):
+				for childFixer in child.getChildren():
+					if childFixer.getText() != '=':
+						varNum.append(childFixer.getText())
+
+			if isinstance(child , StochmizeParser.RandomContext):
+				for childRandom in child.getChildren():
+					if isinstance(childRandom , StochmizeParser.NormalContext):
+						normal_params_m.append(childRandom.normal_params().NUMBER(0).getText())
+						normal_params_v.append(childRandom.normal_params().NUMBER(1).getText())
+
+					if isinstance(childRandom , StochmizeParser.UnifContext):
+						unif_params_a.append(childRandom.unif_params().NUMBER(0).getText())
+						unif_params_b.append(childRandom.unif_params().NUMBER(1).getText())
+		print("==========================")
+
+
 def main():
 
 	lexer = StochmizeLexer(FileStream('input.sz'))
 	stream = CommonTokenStream(lexer)
 	parser = StochmizeParser(stream)
 	tree = parser.program()
-	varsd = parser.vars_def()
+	#varsd = parser.vars_def()
 
 	"""
 	print(tree.ID(0))
@@ -28,9 +60,15 @@ def main():
 
 	print()
 	"""
-	printer = StochmizeListener()
+	printer = StochmizePrintListener()
 	walker = ParseTreeWalker()
 	walker.walk(printer, tree)
+	print(varIds)
+	print(varNum)
+	print(normal_params_m)
+	print(normal_params_v)
+	print(unif_params_a)
+	print(unif_params_b)
 	"""
 	printer = StochmizeListener()
 	res=printer.enterProgram(tree)
